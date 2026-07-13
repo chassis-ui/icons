d56b0a29b05f2fc2b7175a7da746e3d413593c45
Ozgur Gunes <o.gunes@gmail.com>
2026-07-13 17:54:27 +0300

    fix dev server & bump chassis-docs

--- site/src/libs/config.ts
+++ site/src/libs/config.ts
@@ -24,7 +24,8 @@ const configSchema = z.object({
     source: z.url()
   }),
   example_icon: z.string(),
-  github_org: z.url(),
+  figma_handle: z.string(),
+  github_org: z.string(),
   repo: z.url(),
   subtitle: z.string(),
   title: z.string(),
@@ -32,7 +33,7 @@ const configSchema = z.object({
     min: z.number(),
     max: z.number()
   }),
-  x: z.string()
+  x_username: z.string()
 })
 
 let config: Config | undefined
